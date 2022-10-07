54e1eccb6940bb8e6aaac92dc77cccdd13b7c048
Karl <enriquezkharl14@gmail.com>
2022-10-07 12:58:27 +0800

    Update form

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,39 @@
-export const fetchUsers = async (path: RequestInfo) => {
-   const response = await fetch(path, { method: "GET", credentials: "same-origin", headers: { "Content-Type": "application/json" } });
+import User from "../models/user";
 
-   return await response.json();
+export const fetchUsers = async (path: RequestInfo) => {
+   try {
+      const response = await fetch(path, { method: "GET" });
+
+      return await response.json();
+   } catch (err) {
+      console.log(err);
+   }
+};
+
+export const addUser = async (path: RequestInfo, data?: User) => {
+   try {
+      const response = await fetch(path, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data) });
+      return await response.json();
+   } catch (err) {
+      console.log(err);
+   }
+};
+
+export const updateUser = async (path: RequestInfo, data?: User) => {
+   try {
+      const response = await fetch(path, { method: "PUT", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data) });
+      return await response.json();
+   } catch (err) {
+      console.log(err);
+   }
+};
+
+export const deleteUser = async (path: RequestInfo) => {
+   try {
+      const response = await fetch(path, { method: "DELETE" });
+
+      return await response.text();
+   } catch (err) {
+      console.log(err);
+   }
 };
